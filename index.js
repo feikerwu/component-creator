f110e8460676f77fe9f396db56575108fd7783ea
minfield <reFugd?3e@gmail.com>
2018-09-15 15:55:46 +0800

    creator v0.0.1

--- index.js
+++ index.js
@@ -1,1 +1,58 @@
 #!/usr/bin/env node
+const fs = require('fs')
+const path = require('path')
+const config = require(resolve('./config'))
+
+const resolve = (...filePaths) => path.resolve(process.cwd(), ...filePaths)
+const noop = () => {}
+
+/**
+ * 判断一个文件夹是否存在
+ * @param {String} path 文件路径
+ */
+const isExist  = function(path) {
+    try {
+        fs.accessSync(path, fs.constants.F_OK)
+        return true
+    } catch (err) {
+        return false
+    }
+}
+
+/**
+ *  新建文件夹
+ * @param {String} dir 文件夹名字
+ */
+const mkdir = function(dir) {
+    isExist(dir) ? noop() : fs.mkdirSync(dir)
+}
+
+/**
+ * 文件名
+ * @param {String} filename 文件名字
+ */
+const writeFile = function(filename) {
+    if (!isExist(filename)) {
+        let template = fs.createReadStream(path.resolve(__dirname, 'template.vue'))
+        let component = fs.createWriteStream(filename)
+        template.pipe(component)
+    }
+}
+
+module.exports = {
+    isExist,
+    mkdir,
+    writeFile
+}
+
+
+config.forEach(item => {
+    let { dir, components } = item
+    dir = dir ? dir : './'
+
+    mkdir(resolve(dir))
+    components.forEach(component => {
+        writeFile(resolve(dir, `component`))
+    })
+})
+
